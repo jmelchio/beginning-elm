597516547a3c9c2a66e9a7ccedebf9c731449f19
Joris Melchior <joris.melchior@gmail.com>
2017-12-11 23:30:51 -0500

    More stuff in Playground.elm

--- elm-examples/Playground.elm
+++ elm-examples/Playground.elm
@@ -3,14 +3,83 @@ module Playground exposing (..)
 import Html
 
 
-escapeEarth : Float -> Float -> String
-escapeEarth velocity speed =
-    if velocity > 11.186 then
+weekday : Int -> String
+weekday dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+
+hashtag : Int -> String
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+
+escapeEarth : String -> Float -> Float -> String
+escapeEarth fuelStatus velocity speed =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand fuelStatus =
+            if fuelStatus == "low" then
+                "Land on droneship"
+            else
+                "Land on launchpad"
+    in
+    if velocity > escapeVelocityInKmPerSec then
         "Godspeed"
-    else if speed == 7.67 then
+    else if speed == orbitalSpeedInKmPerSec then
         "Stay in orbit"
     else
-        "Come back"
+        whereToLand fuelStatus
 
 
 speed : Float -> Int -> Float
@@ -28,7 +97,7 @@ main =
     -- Html.text (escapeEarth 11 (speed 7.67 (time 2 3)))
     time 2 3
         |> speed 7.67
-        |> escapeEarth 11
+        |> escapeEarth "low" 11
         |> Html.text
 
 
