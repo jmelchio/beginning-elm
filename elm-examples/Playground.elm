a96cad9a01ec527a8f4175113f18107d961e3685
Joris Melchior <joris.melchior@gmail.com>
2017-12-11 23:43:10 -0500

    Yeah, there's more

--- elm-examples/Playground.elm
+++ elm-examples/Playground.elm
@@ -3,6 +3,23 @@ module Playground exposing (..)
 import Html
 
 
+revelation : String
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+    """
+
+
+msFunRunftc : String
+msFunRunftc =
+    "Michael Scott's Rabies Awareness \"Fun Run\" Race for the Cure"
+
+
 weekday : Int -> String
 weekday dayInNumber =
     case dayInNumber of
